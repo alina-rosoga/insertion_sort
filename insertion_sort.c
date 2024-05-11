8b6a44b02b907fe2c5c988af7dc5cca5235ea64f
alina-rosoga <alina.rosoga04@e-uvt.ro>
2024-05-12 00:04:03 +0300

    Sortare prin insertie

---
+++ insertion_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    const int size = 10000; // Array size
+    int arr[size];
+
+    srand(time(NULL));
+
+    printf("Initial array:\n");
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10000;
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    insertionSort(arr, size);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double time_taken = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Sorted array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("Execution time: %.15f seconds\n", time_taken);
+
+    return 0;
+}
